035f8dedd75dd2557556265c47a3e2cc7556a012
Adi Shenkman <31403740+AdiSh01@users.noreply.github.com>
2017-09-25 16:48:30 +0300

    BlockScreen component with mamtak image

--- app/ts/app.module.ts
+++ app/ts/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import {BankResponsibilityComponent} from './bank-responsibility.component';
+import {BlockScreenComponent} from "./block-screen.component";
 
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent , BankResponsibilityComponent ],
+  declarations: [ AppComponent , BankResponsibilityComponent, BlockScreenComponent ],
   bootstrap:    [ AppComponent ]
 })
 
---
+++ app/ts/block-screen.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    selector: 'block-screen',
+    templateUrl: 'app/ts/block-screen.component.html',
+})
+
+export class BlockScreenComponent{
+}
