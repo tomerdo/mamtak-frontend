9e64d4e6ba3411c5ffb9449363c2900adc38d618
tomerdo <tomerdo@post.bgu.ac.il>
2017-09-25 09:32:37 +0300

    added function of slow resolving for the example.

--- app/ts/accounts-responsibility.service.ts
+++ app/ts/accounts-responsibility.service.ts
@@ -4,8 +4,16 @@ import { RESPONSIBILITIES } from './mock-responsibilities';
 
 @Injectable()
 export class AccountsResponsibilityService {
+
     getAccountsResponsibility(): Promise<AccountsResponsibility[]>{
         return Promise.resolve(RESPONSIBILITIES);
+    }
+
+    getAccountsResponsibilitySlowly(): Promise<AccountsResponsibility[]> {
+      return new Promise(resolve => {
+        // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(this.getAccountsResponsibility()), 2000);
+      });
     }
 
 }
