19f5bfb53d8b771700cace1371f4733bea1f2f48
cleanskyiaf <cleanskyiaf@gmail.com>
2017-09-26 19:15:35 +0300

    adding suprvisedAccount to modal

--- app/ts/accounts-responsibility.ts
+++ app/ts/accounts-responsibility.ts
@@ -7,8 +7,11 @@ export class AccountsResponsibility{
     maxCreditValue:number;
     moneyChannel:number;
     otherNotes:string;
+    supervisedAccounts: Array<AccountsResponsibility>
 
-    constructor(id:number, accountsArea:string, adminName:string, minCreditValue:number, maxCreditValue:number, moneyChannel:number, otherNotes:string )
+    
+
+    constructor(id:number, accountsArea:string, adminName:string, minCreditValue:number, maxCreditValue:number, moneyChannel:number, otherNotes:string, supervisedAccounts: Array<AccountsResponsibility> = [])
     {
         this.id = id;
         this.accountsArea = accountsArea;
@@ -17,6 +20,7 @@ export class AccountsResponsibility{
         this.maxCreditValue = maxCreditValue;
         this.moneyChannel = moneyChannel;
         this.otherNotes = otherNotes;
+        this.supervisedAccounts = supervisedAccounts;
     }
 
 }
